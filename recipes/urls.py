d2bd6a6582612c5d7392854873b948877cd9cc90
Bruno Amorim <brunoamorim1706@gmail.com>
2024-04-29 20:35:54 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
-from recipes.views import home, sobre, contato
+from recipes.views import home
 from django.urls import path
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+   
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,9 +6,3 @@ def home(resquest):
     return render(resquest, 'recipes/home.html')
 
 
-def sobre(resquest):
-    return HttpResponse('Sobre')
-
-
-def contato(resquest):
-    return HttpResponse('Contato')
