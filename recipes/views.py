cb2793f511b1a722b561a8f0a2e3ef349268b832
Bruno Amorim <brunoamorim1706@gmail.com>
2024-04-30 18:22:20 -0300

    head html

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(resquest):
-    return render(resquest, 'recipes/home.html')
+    return render(resquest, 'recipes/pages/home.html')
 
 
