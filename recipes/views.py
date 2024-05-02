1b0f08c275f2cba852283d62ce8d8177934142c9
Bruno Amorim <brunoamorim1706@gmail.com>
2024-05-02 04:51:52 -0300

    factory

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(resquest):
     return render(resquest, 'recipes/pages/home.html', context={
-        'name': 'Bruno Amorim',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(resquest, id):
     return render(resquest, 'recipes/pages/recipe-view.html', context={
-        'name': 'Bruno Amorim',
+        'recipe': make_recipe,
+        'is_detail_page': True,
     })
