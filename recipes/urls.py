b34e93bbec2f906545b4ed4b44484968f9854306
Bruno Amorim <brunoamorim1706@gmail.com>
2024-05-02 03:08:58 -0300

    separando templates

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
-from recipes.views import home
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    path('', home),
-   
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(resquest):
-    return render(resquest, 'recipes/pages/home.html')
+    return render(resquest, 'recipes/pages/home.html', context={
+        'name': 'Bruno Amorim',
+    })
 
 
+def recipe(resquest, id):
+    return render(resquest, 'recipes/pages/recipe-view.html', context={
+        'name': 'Bruno Amorim',
+    })
