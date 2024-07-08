4c7f14a1647ee93bd9254aa8959135b43958fca2
Bruno Amorim <brunoamorim1706@gmail.com>
2024-07-08 16:50:29 -0300

    migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class recipe(models.Model):
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
@@ -14,4 +20,5 @@ class recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
