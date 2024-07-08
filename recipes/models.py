03ae194bde43de0aae4a5e32b392a8df2eb74f0d
Bruno Amorim <brunoamorim1706@gmail.com>
2024-07-08 19:55:31 -0300

    django admin

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
